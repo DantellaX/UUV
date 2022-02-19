c4ab432338cf6ce5f9120d0629a4f2b94a574285
DantellaX <98627758+DantellaX@users.noreply.github.com>
2022-02-18 17:22:05 -0800

    Update readParameters.py

--- test/readParameters.py
+++ test/readParameters.py
@@ -5,17 +5,25 @@ import math
 # Import mavutil
 from pymavlink import mavutil
 
+g = 9.81 # m/s^2
+rho_water = 997 # kg/m^3
+pressure_air = 101325 # Pa
+
 
 # Create the connection
 master = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
 # Wait a heartbeat before sending commands
 master.wait_heartbeat()
 
-print('yaw: ' + str(math.degrees(master.recv_match(type='ATTITUDE', blocking=True).to_dict().get('yaw'))))
+def getDepth():
+    pressure = master.recv_match(type='SCALED_PRESSURE', blocking=True).to_dict().get('press_abs') * 100.0
+    h = (pressure_air - pressure) /(rho_water * g)
+    return str(round(h,3))
 
+print('yaw: ' + str(round(math.degrees(master.recv_match(type='ATTITUDE', blocking=True).to_dict().get('yaw')), 3)))
+'''
 print('x: ' + str(master.recv_match(type='LOCAL_POSITION_NED', blocking=True).to_dict().get('x')))
 print('y: ' + str(master.recv_match(type='LOCAL_POSITION_NED', blocking=True).to_dict().get('y')))
-print('z: ' + str(-master.recv_match(type='LOCAL_POSITION_NED', blocking=True).to_dict().get('z')))
-
-print('altitude: ' + str(master.recv_match(type='GLOBAL_POSITION_INT', blocking=True).to_dict().get('relative_alt')/1000.0))
-
+'''
+print('altitude: ' + getDepth())
+print('pressure: ' + str(round(master.recv_match(type='SCALED_PRESSURE', blocking=True).to_dict().get('press_abs') * 100.0, 3)))
